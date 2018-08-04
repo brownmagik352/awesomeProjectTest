7f697ebbc959f3e391845d8729fd7a1473c66804
Apurv Suman <sumanicus@gmail.com>
2018-08-04 10:23:52 -0700

    call now sets up phone call

--- components/ActionableContact.js
+++ components/ActionableContact.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import call from 'react-native-phone-call';
 
 export default class ActionableContact extends Component {
     render() {
         return (
             <View style={styles.singleRow}>
                 <Text>{this.props.name}</Text>
-                <Button title="Call" onPress={() => console.log("Call " + this.props.name + "(" + this.props.number + ")")} />
+                <Button title="Call" onPress={() => call({number: this.props.number, prompt: true}).catch(console.error)} />
                 <Button title="Text" onPress={() => console.log("Text " + this.props.name + "(" + this.props.number + ")")} />
             </View>
         )
