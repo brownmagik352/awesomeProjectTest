fa006a51dfdb3956cbff6fad22d0075fe2ff5932
Apurv Suman <sumanicus@gmail.com>
2018-08-13 08:57:09 -0700

    code cleanup

--- components/Main.js
+++ components/Main.js
@@ -14,35 +14,23 @@ const styles = StyleSheet.create({
   },
 });
 
-// TODO: move back into static functions
-
-// given a phone number in any string format, strips it down to just digits
-function stripNonDigitsFromPhoneNumber(phoneNumberString) {
-  const validNumChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  const numberStringArray = phoneNumberString.split('');
-  const badCharsIndicesArray = [];
-  // find all the indices where there isn't a number in the phoneNumberString
-  for (let i = 0; i < numberStringArray.length; i += 1) {
-    if (!validNumChars.includes(numberStringArray[i])) badCharsIndicesArray.push(i);
-  }
-  // remove all bad chars
-  for (let i = badCharsIndicesArray.length - 1; i >= 0; i -= 1)
-    numberStringArray.splice(badCharsIndicesArray[i], 1);
-
-  return numberStringArray.join('');
-}
-
-// TODO: handle getting name in all cases (names missing etc.)
-function getName(contact) {
-  return `${contact.givenName} ${contact.familyName}`;
-}
-
-// TODO: handle getting name in all cases (names missing etc.)
-function getNumber(contact) {
-  return stripNonDigitsFromPhoneNumber(contact.phoneNumbers[0].number);
-}
-
 export default class Main extends Component {
+  // given a phone number string to just digits
+  static stripNonDigitsFromPhoneNumber(phoneNumberString) {
+    const validNumChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const numberStringArray = phoneNumberString.split('');
+    const badCharsIndicesArray = [];
+    // find all the indices where there isn't a number in the phoneNumberString
+    for (let i = 0; i < numberStringArray.length; i += 1) {
+      if (!validNumChars.includes(numberStringArray[i])) badCharsIndicesArray.push(i);
+    }
+    // remove all bad chars
+    for (let i = badCharsIndicesArray.length - 1; i >= 0; i -= 1)
+      numberStringArray.splice(badCharsIndicesArray[i], 1);
+
+    return numberStringArray.join('');
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -62,6 +50,16 @@ export default class Main extends Component {
 
     // get stored reminders
     this.retrieveReminders();
+  }
+
+  // TODO: handle getting name in all cases (names missing etc.)
+  static getName(contact) {
+    return `${contact.givenName} ${contact.familyName}`;
+  }
+
+  // TODO: handle getting name in all cases (names missing etc.)
+  static getNumber(contact) {
+    return Main.stripNonDigitsFromPhoneNumber(contact.phoneNumbers[0].number);
   }
 
   // assigns a new random ID
@@ -107,9 +105,9 @@ export default class Main extends Component {
       PushNotification.localNotificationSchedule({
         id: `${contact.id}`,
         message: contact.name, // (required)
-        date: new Date(Date.now() + 10 * 1000),
+        date: new Date(Date.now() + 5 * 1000),
         repeatType: 'time',
-        repeatTime: 1000 * 10,
+        repeatTime: 5 * 1000,
       })
     );
   }
@@ -208,11 +206,11 @@ export default class Main extends Component {
             data={contacts}
             renderItem={({ item }) => (
               <ContactListItem
-                name={getName(item)}
+                name={Main.getName(item)}
                 onPress={() =>
                   this.handleContactPress({
-                    name: getName(item),
-                    number: getNumber(item),
+                    name: Main.getName(item),
+                    number: Main.getNumber(item),
                     id: this.getNewReminderId(),
                   })
                 }
